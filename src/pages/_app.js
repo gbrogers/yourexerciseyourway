0a09c53deeea07bbc0648908a7bcdd4e8c9c6e9d
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-11-08 21:10:24 -0700

    useContext implemented

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,24 @@
 import "../styles/globals.css";
 import Layout from "../../components/Layout/Layout";
+import React, { useState, useMemo } from "react";
+import { UserContext } from "../UserContext";
+import { AuthContext } from "../AuthContext";
 
 function MyApp({ Component, pageProps }) {
+  const [user, setUser] = useState(null);
+  const [isAuth, setIsAuth] = useState(false);
+
+  const userValue = useMemo(() => ({ user, setUser }), [user, setUser]);
+  const authValue = useMemo(() => ({ isAuth, setIsAuth }), [isAuth, setIsAuth]);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <UserContext.Provider value={userValue}>
+      <AuthContext.Provider value={authValue}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthContext.Provider>
+    </UserContext.Provider>
   );
 }
 
