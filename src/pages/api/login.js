6ccf1080b7a9945c758efc9091157bc874e52cac
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-11-10 19:27:46 -0700

    EOD

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,22 +1,4 @@
-import prisma from "../../../db/index";
-
 export default async function (req, res) {
   console.log(req.body);
   const { fname, lname, email, password } = req.body;
-
-  const user = await prisma.user
-    .create({
-      data: {
-        email,
-        fname,
-        lname,
-        password,
-      },
-    })
-    .then((res) => {
-      res.status(200).json({ user });
-    })
-    .catch(async (error) => {
-      console.log(error);
-    });
 }
--- src/pages/api/signup.js
+++ src/pages/api/signup.js
@@ -1,3 +1,7 @@
+const User = require("../../../server/db/models/user");
+const { Op } = require("sequelize");
+const bcrypt = require("bcryptjs");
+
 export default function handler(req, res) {
   console.log("in handler");
   res.status(200).json({ name: "John Doe" });
